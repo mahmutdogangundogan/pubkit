dedda7a940555e029cde488c71cce774f004cf81
Mahmut Doğan Gündoğan <gundoganmd19@gmail.com>
2026-04-22 19:21:06 +0300

    create edefault exports for publication models

---
+++ src/pubkit/models/__init__.py
@@ -0,0 +1,47 @@
+from .publication import (
+    # Base
+    ContentNode,
+    # Content nodes
+    TextRun,
+    Math,
+    Image,
+    Link,
+    Reference,
+    Footnote,
+    Formula,
+    DocListItem,
+    DocList,
+    # Document structure
+    Paragraph,
+    Figure,
+    TableCell,
+    TableRow,
+    TableItem,
+    Table,
+    ChemExp,
+    Section,
+    Publication,
+)
+
+__all__ = [
+    # Domain Models
+    "ContentNode",
+    "TextRun",
+    "Math",
+    "Image",
+    "Link",
+    "Reference",
+    "Footnote",
+    "Formula",
+    "DocListItem",
+    "DocList",
+    "Paragraph",
+    "Figure",
+    "TableCell",
+    "TableRow",
+    "TableItem",
+    "Table",
+    "ChemExp",
+    "Section",
+    "Publication"
+]
